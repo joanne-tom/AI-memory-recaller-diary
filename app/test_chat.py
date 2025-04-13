a5e514282d61f21b1a437c08f4cc354ffce3127d
Joanne Rachel Tom <133677457+joanne-tom@users.noreply.github.com>
2025-04-13 19:51:20 +0530

    Delete app/test_chat.py

--- app/test_chat.py
+++
@@ -1,7 +0,0 @@
-from memory_retriever import chat_with_user
-
-user_id = "joannetom22@gmail.com"
-user_message = "I'm feeling very sad today."
-
-response = chat_with_user(user_id, user_message)
-print("Chat Response:", response)
